e2fe246fa0d8a88a1f2a95a753a6d1704dc53cb1
Danielreyezzz <dangioparefape@gmail.com>
2023-03-17 10:38:15 +0100

    Navbar reactivo a scroll

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,13 @@ function reveal() {
   }
   window.addEventListener("scroll", reveal);
   reveal();
+
+
+const activePage = window.location;
+const navLinks = document.querySelectorAll('.nav-link').
+forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add('active');
+  }
+})
+
